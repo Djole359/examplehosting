a411105ae527a0f0fe57b3ed1ead601e93c821e4
Djole359 <djordjemilovancev09@gmail.com>
2025-07-15 17:53:51 +0200

    Add files via upload

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ui import Button, View
 import os
+import webserver
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -56,6 +57,7 @@ async def on_interaction(interaction: discord.Interaction):
                 ephemeral=True
             )
 
+webserver.keep_alive()
 bot_token = os.getenv("MTM4MzAwODQ2ODgxODIwMjY1NA.GkCJUJ.Ss1r5RDzPer3jL-fYuQsIrSJMqa0wTi3-W6a_8")
 if not bot_token:
     raise ValueError("DISCORD_BOT_TOKEN environment variable not set.")
